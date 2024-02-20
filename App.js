bfe5be03630e4b0637ed5e6a90ef67cf9b5b9cc2
Zin Min Htet <zinminhtet.dev@gmail.com>
2024-02-20 18:06:52 +0630

    Build a screen

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import ImageViewer from "./components/ImageViewer";
+import Button from "./components/Button";
+
+const PlaceholderImage = require("./assets/images/background-image.png");
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<View style={styles.imageContainer}>
+				<ImageViewer PlaceholderImageSource={PlaceholderImage} />
+			</View>
+			<View style={styles.footerContainer}>
+				<Button label="Choose a photo" theme="primary" />
+				<Button label="Use this photo" />
+			</View>
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#25292e",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	imageContainer: {
+		flex: 1,
+		paddingTop: 58,
+	},
+	footerContainer: {
+		flex: 1 / 3,
+		alignItems: "center",
+	},
 });
